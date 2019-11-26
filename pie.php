9f233e10ce3be50d3efb706f38bf8a123ac0ebbc
cece <celialopezdelermaalarcon@gmail.com>
2019-11-26 12:52:52 +0100

    Creación del footer

---
+++ pie.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<footer>
+    <a href="#">¿Quiénes somos?</a>
+    <a href="#">Contacto</a>
+    <img id="icono" src="http://simpleicon.com/wp-content/uploads/facebook.png" alt="logo facebook">
+    <img id="icono" src="http://www.vectorico.com/download/social_media/Instagram-Logo.png" alt="logo instagram">
+    <img id="icono" src="http://pixsector.com/cache/f8dbbdb2/avb37443a9d16e06eef66.png" alt="logo twitter">
+    <br>
+    <p>Realizado por Celia López de Lerma Alarcón</p>
+    
+</footer>
+<?php
+?>
